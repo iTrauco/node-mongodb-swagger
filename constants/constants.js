3fda5ca2f8e7e243a2f5325a1d4a5d3c5e31edfc
Christopher Trauco <Chris@Trau.co>
2019-06-25 06:45:31 -0400

    configured MongoDB w/ statuses

---
+++ constants/constants.js
@@ -0,0 +1,10 @@
+module.exports = {
+    databaseStatus: {
+        ENTITY_CREATED: 'Entity Created',
+        ENTITY_MODIFIED: 'Entity Modified',
+        ENTITY_FETCHED: 'Entity Fetched',
+        ENTITY_DELETED: 'Entity Deleted',
+        DATABASE_CONNECTED: 'Successfully connected to MongoDB',
+        DATABASE_ERROR: 'Database error'
+    }
+}
--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 //////=========================================================================
 //////=========================================================================
+// MODULES
 // LOAD EXPRESS
 const express = require('express');
 const app = express();
 // LOAD BODY-PARSER
 const bodyParser = require('body-parser');
-// CONFIGURE BODY-PARSER
+// LOAD DOTENV
+const dotEnv = require('dotenv').config();
+// LOAD CORS
+const cors = require('cors');
+// LOAD CRUDREPO
+const crudRepository = require('./database/crudRepository');
+// CONFIGURE APP
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true})); // 'extended' property allows for the passing of nested objects
-// LOAD DOTENV
-const dotEnv = require('dotenv').config()
-// LOAD CORS | CONFIGURE CORS
-const cors = require('cors');
-app.use(cors())
 
+crudRepository.createConnection();
+
+app.use(cors());
 // PORT
 const PORT = process.env.PORT || 3434;
 //
-//
+////
 //////=========================================================================
 //////=========================================================================
 // CONFIGURE ROUTES
 //
 app.use('/', (req, res, next) => {
-    res.send('Hello from the main/primary Node API endpoint...');
+    res.send('Main end point...');
 })
 //
-//
+////
 //////=========================================================================
 //////=========================================================================
 // INITIALIZE SERVER
