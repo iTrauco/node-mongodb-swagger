1964e212a32a24dd51a2859df8e5c575a4149a17
Christopher Trauco <Chris@Trau.co>
2019-06-25 06:06:02 -0400

    Installed CORS | cleanup coding format

--- index.js
+++ index.js
@@ -8,20 +8,29 @@ const bodyParser = require('body-parser');
 // CONFIGURE BODY-PARSER
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true})); // 'extended' property allows for the passing of nested objects
+// LOAD DOTENV
+const dotEnv = require('dotenv').config()
+// LOAD CORS | CONFIGURE CORS
+const cors = require('cors');
+app.use(cors())
 
-const PORT = 3333;
-
+// PORT
+const PORT = process.env.PORT || 3434;
+//
+//
 //////=========================================================================
 //////=========================================================================
 // CONFIGURE ROUTES
+//
 app.use('/', (req, res, next) => {
     res.send('Hello from the main/primary Node API endpoint...');
 })
-
-
+//
+//
 //////=========================================================================
 //////=========================================================================
-// CONFIGURE SERVER
+// INITIALIZE SERVER
+//
 app.listen(PORT, () => {
     console.log(`Now listening on port ${PORT}...`)
 })
